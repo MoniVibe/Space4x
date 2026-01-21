324f600071d531baf41c149861083141a3d28902
MoniVibe <shonhay@gmail.com>
2026-01-21 18:30:25 +0200

    nightly: arc_orientation_stub

--- Assets/Scripts/Space4x/Headless/Space4XHeadlessDiagnosticsSystem.cs
+++ Assets/Scripts/Space4x/Headless/Space4XHeadlessDiagnosticsSystem.cs
@@ -119,6 +119,7 @@ namespace Space4X.Headless
                 {
                     _runStarted = 1;
                     Space4XHeadlessDiagnostics.UpdateProgress("run", "start", tick);
+                    Space4XHeadlessDiagnostics.UpdateProgress("run", "start", tick);UnityEngine.Debug.Log("[Anviloop] ARC_START_STUB");
                 }
 
                 if (_runCompleted == 0 && runtime.EndTick > 0 && tick >= runtime.EndTick)
