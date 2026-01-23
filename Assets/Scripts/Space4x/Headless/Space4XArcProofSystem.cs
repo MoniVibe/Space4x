2daf27e8b9024aefd91b1fa8f4a47f8d17bbe190
MoniVibe <shonhay@gmail.com>
2026-01-24 00:53:06 +0200

    [night] Fix ArcProofSystem scenario namespace casing

--- Assets/Scripts/Space4x/Headless/Space4XArcProofSystem.cs
+++ Assets/Scripts/Space4x/Headless/Space4XArcProofSystem.cs
@@ -5,7 +5,7 @@ using PureDOTS.Runtime.Scenarios;
 using PureDOTS.Runtime.Telemetry;
 using PureDOTS.Runtime.Time;
 using Space4X.Registry;
-using Space4X.Scenario;
+using Space4x.Scenario;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
